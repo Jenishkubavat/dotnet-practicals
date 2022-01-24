d8351a7db52f42fdc844d187b633ec034dca3393
jenishkubavat <dante2709201@gmail.com>
2022-01-24 16:17:34 +0530

    Assignment no.2

---
+++ Asssignment 1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var names = new[] { "jenish", "Dante", "Emillia" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
